afa421bc9bc29e04e45010f1700b1d9f9e977ca2
andresu13 <andresu13>
2023-03-16 01:32:29 +0000

    updated Azure DevOps notebook

--- 02_AZDevOps.py
+++ 02_AZDevOps.py
@@ -1,2 +1,7 @@
 # Databricks notebook source
-
+# MAGIC %md
+# MAGIC ### Configure Azure DevOps
+# MAGIC 
+# MAGIC This section of the Solution Accelerator will be done outside Databricks. We are using Azure DevOps as the CI/CD tool to download, test and push a Docker image containing the ML model to an Azure Container Registry. Although we are leveraging this Azure services to build the automation, the pattern we are presenting here does not depend on any of those services, and other CI/CD solutions and/or image repositories such as Jenkins and Docker registry are fully supported as well.
+# MAGIC 
+# MAGIC The image below shows where Azure DevOps fits in the overall ML edge deployment pipeline:
