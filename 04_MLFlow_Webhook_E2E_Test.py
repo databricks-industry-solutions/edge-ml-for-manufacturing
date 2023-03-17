ecf1e471634cfeccff7f2dc48b6dca6435ab37ed
dbbnicole <dbbnicole>
2023-03-17 15:48:02 +0000

    Combine notebook 04 and 05 so that we can avoid accumulating webhooks
    And 
    * Update README package legal table
    * Add image for verifying successful deployment

--- 04_MLFlow_Webhook_E2E_Test.py
+++ 04_MLFlow_Webhook_E2E_Test.py
@@ -102,5 +102,51 @@ RegistryWebhooksClient().list_webhooks(model_name=model_name)
 
 # COMMAND ----------
 
-# Uncomment code below to delete any webhook by providing the webhook id
-# RegistryWebhooksClient().delete_webhook(id=job_webhook.id)
+# MAGIC %md Now that we have everything in place, let's proceed to test out the deployment.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #End-to-end Testing
+# MAGIC One of the benefits of using MLflow is the model versioning capabilities that it provides. Every time a new run is registered to an existing model in MLflow, a new version of that model is automatically created. This allows ML engineers to track and monitor the performance of new model versions and compare that to older versions. If a new version performs better with new data being received, they can promote that version to “Production” and automatically push that through the CI/CD pipeline that deployed these models to the edge. The same applies to scenarios where a new model version is not performing as expected and we need to roll back to a previous working version of the model.
+# MAGIC 
+# MAGIC <img src="https://github.com/databricks-industry-solutions/edge-ml-for-manufacturing/blob/main/images/model_versions_mlflow.png?raw=true" width=75%/>
+# MAGIC 
+# MAGIC 
+# MAGIC We tag the Docker images with the version of the model that was used to create the image. Using that approach, we can easily keep track of the model versions deployed to edge devices and either update or rollback as conditions in the field change and model drift occurs.
+
+# COMMAND ----------
+
+import mlflow
+from mlflow.tracking.client import MlflowClient
+
+# COMMAND ----------
+
+client = MlflowClient()
+
+model_details = client.get_latest_versions(model_name)[0]
+
+client.transition_model_version_stage(
+  name=model_details.name,
+  version=model_details.version,
+  stage='Production',  
+  archive_existing_versions=True
+)
+
+# COMMAND ----------
+
+# MAGIC %md This model stage transition should have triggered a run in the Azure DevOps Pipeline we deployed. A successful run may look like:
+# MAGIC 
+# MAGIC <img src="https://github.com/databricks-industry-solutions/edge-ml-for-manufacturing/blob/main/images/azdevops-pipeline-run.png?raw=true" width=75%/>
+# MAGIC 
+# MAGIC You should also verify the container deployment in the container registry.
+# MAGIC 
+# MAGIC After testing this deployment pattern, if you not intend to leave the webhook around, make sure to clean it up.
+
+# COMMAND ----------
+
+# DBTITLE 0,After the webhook has been tested, let's clean it up.
+import time
+time.sleep(60)
+
+RegistryWebhooksClient().delete_webhook(id=job_webhook.id)
